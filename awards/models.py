fc95d628657f9bdb6db535326c7a76c2faa5cd4c
Jerry Arciaga <jerryarciaga11@gmail.com>
2020-06-15 05:58:11 +0100

    Added model Awardee to app

--- awards/models.py
+++ awards/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Awardee(models.Model):
+    first_name = models.CharField(max_length=50)
+    middle_initial = models.CharField(max_length=1, blank=True)
+    last_name = models.CharField(max_length=50)
+    award = models.CharField(max_length=5)
+
+    def __str__(self):
+        if middle_initial:
+            name = f'{self.last_name}, {self.first_name} {self.middle_initial}.'
+        else:
+            name = f'{self.last_name}, {self.first_name}'
+        
+        return f'{name} - {award}'
