e073dc51584dca921ea6c780e2baeb0414995b4e
Jerry Arciaga <jerryarciaga11@gmail.com>
2020-06-20 17:23:33 -0600

    Added periods of award and presentation date to Awardee model

--- awards/models.py
+++ awards/models.py
@@ -2,10 +2,17 @@ from django.db import models
 
 # Create your models here.
 class Awardee(models.Model):
+    # Recipient data
     first_name = models.CharField(max_length=50)
     middle_initial = models.CharField(max_length=1, blank=True)
     last_name = models.CharField(max_length=50)
+
+    # Award data
     award = models.CharField(max_length=5)
+    from_date = models.DateField(null=True)
+    to_date = models.DateField(null=True)
+    presentation_date = models.DateField(null=True)
+    
 
     def __str__(self):
         if self.middle_initial:
