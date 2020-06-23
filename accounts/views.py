9efb3cc954f75b0008013dbea9f24864f49539be
Jerry Arciaga <jerryarciaga11@gmail.com>
2020-06-23 10:42:54 -0600

    Added urls and views to render account sign up and login

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 
+def signup(request):
+    return render(request, 'accounts/signup.html')
 
-
+def login(request):
+    return render(request, 'accounts/login.html')
