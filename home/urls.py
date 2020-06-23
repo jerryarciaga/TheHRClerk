a6fe887f524c2b1f197a93bc64d8fcbd14a76946
Jerry Arciaga <jerryarciaga11@gmail.com>
2020-06-23 12:20:50 -0600

    Added url and view for the Help Page

--- home/urls.py
+++ home/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'home'
 urlpatterns = [
     url(r'^$', views.home, name='home'),
-    url(r'^about/', views.about, name='about')
+    url(r'^help/', views.help, name='help'),
+    url(r'^about/', views.about, name='about'),
 ]
