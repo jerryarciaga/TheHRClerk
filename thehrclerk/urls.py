a1bc59cfcfbb84ea515b22301f8a4de29d37876a
Jerry Arciaga <jerryarciaga11@gmail.com>
2020-06-23 10:44:14 -0600

    Added url to accounts

--- thehrclerk/urls.py
+++ thehrclerk/urls.py
@@ -19,5 +19,6 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('home.urls')),
+    url(r'^accounts/', include('accounts.urls')),
     url(r'^awards/', include('awards.urls')),
 ]
