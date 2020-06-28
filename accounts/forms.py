f9dd8d40d2f83a6837102c901fc986dd2c231b50
Jerry Arciaga <jerryarciaga11@gmail.com>
2020-06-27 23:34:29 -0600

    Capitalized form labels

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,9 +3,11 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 class SignUpForm(UserCreationForm):
-    first_name = forms.CharField(max_length=30,
+    first_name = forms.CharField(label="First Name:",
+                                max_length=30,
                                 required=True)
-    last_name = forms.CharField(max_length=30,
+    last_name = forms.CharField(label="Last Name:",
+                                max_length=30,
                                 required=True)
     email = forms.EmailField(max_length=254,
                                 help_text='Enter a valid email address.')
