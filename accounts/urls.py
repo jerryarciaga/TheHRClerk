eebd790172bfe4a506c9349ec7d05f2d31b7e502
Jerry Arciaga <jerryarciaga11@gmail.com>
2020-06-28 14:58:16 -0600

    Added built in login view

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
 from . import views
 
+from django.contrib.auth import views as auth_views
+
 app_name = 'accounts'
 urlpatterns = [
     url(r'^signup/$', views.signup, name='signup'),
-    url(r'^login/$', views.userlogin, name='login'),
+    url(r'^login/$', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
     url('^logout/$', views.userlogout, name='logout'),
 ]
