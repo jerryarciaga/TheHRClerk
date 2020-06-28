9e9179a8f56df05fc97ad003caf57ddbc030872c
Jerry Arciaga <jerryarciaga11@gmail.com>
2020-06-28 17:52:02 -0600

    Fixed forms and views to properly render Sign In, Log In and Log Out functionalities

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 class SignUpForm(UserCreationForm):
@@ -29,8 +29,3 @@ class SignUpForm(UserCreationForm):
                 'password1',
                 'password2',
                 )
-
-class LoginForm(AuthenticationForm):
-    class Meta:
-        model = User
-        fields = '__all__'
--- accounts/views.py
+++ accounts/views.py
@@ -4,9 +4,11 @@ from django.shortcuts import render, redirect
 from .forms import SignUpForm
 
 def signup(request):
+    # Log any user out when trying to make an account
     if request.user.is_authenticated:
         logout(request)
-
+    
+    # Redirect to home if user successfully signs up
     if request.method == 'POST':
         form = SignUpForm(request.POST)
         if form.is_valid():
@@ -16,15 +18,10 @@ def signup(request):
             user = authenticate(username=username, password=raw_password)
             login(request, user)
             return redirect('home:home')
+    # Render the sign up form
     else:
         form = SignUpForm()
     return render(request, 'accounts/signup.html', {'form' : form})
-
-def userlogin(request):
-    if request.user.is_authenticated:
-        logout(request)
-
-    return render(request, 'accounts/login.html')
 
 def userlogout(request):
     logout(request)
