cb87a4bf4e31b9f0809e24a261973617661383d0
Jerry Arciaga <jerryarciaga11@gmail.com>
2020-07-03 22:46:27 -0600

    Added support to save Profile data on Sign Up

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,10 @@
 from django.contrib.auth import login, logout, authenticate
 from django.shortcuts import render, redirect
+from django.db import transaction
 
-from .forms import SignUpForm
+from .forms import SignUpForm, ProfileForm
 
+@transaction.atomic
 def signup(request):
     # Log any user out when trying to make an account
     if request.user is not None:
@@ -10,18 +12,29 @@ def signup(request):
     
     # Redirect to home if user successfully signs up
     if request.method == 'POST':
-        form = SignUpForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password1')
+        user_form = SignUpForm(request.POST)
+        profile_form = ProfileForm(request.POST)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            username = user_form.cleaned_data.get('username')
+            raw_password = user_form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
             login(request, user)
+            profile_form = ProfileForm(request.POST, instance=request.user.profile)
+            profile_form.save()
             return redirect('home:home')
     # Render the sign up form
     else:
-        form = SignUpForm()
-    return render(request, 'accounts/signup.html', {'form' : form})
+        user_form = SignUpForm()
+        profile_form = ProfileForm()
+    return render(
+        request = request,
+        template_name = 'accounts/signup.html', 
+        context = {
+            'user_form' : user_form,
+            'profile_form' : profile_form,
+        }
+    )
 
 def userlogout(request):
     logout(request)
