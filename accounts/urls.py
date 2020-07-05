d630e6702def8526380fa4c374220795c2c511c4
Jerry Arciaga <jerryarciaga11@gmail.com>
2020-07-04 20:31:26 -0600

    Turned Sign Up view into a class-based view

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,7 +5,7 @@ from django.contrib.auth import views as auth_views
 
 app_name = 'accounts'
 urlpatterns = [
-    url(r'^signup/$', views.signup, name='signup'),
+    url(r'^signup/$', views.SignUpView.as_view(), name='signup'),
     url(r'^login/$', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
     url(r'^logout/$', views.userlogout, name='logout'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,40 +1,46 @@
 from django.contrib.auth import login, logout, authenticate
 from django.shortcuts import render, redirect
+
+from django.views import View
+from django.utils.decorators import method_decorator
 from django.db import transaction
 
 from .forms import SignUpForm, ProfileForm
 
-@transaction.atomic
-def signup(request):
-    # Log any user out when trying to make an account
-    if request.user is not None:
-        logout(request)
+class SignUpView(View):
+    user_form = SignUpForm
+    profile_form = ProfileForm
+    template_name = 'accounts/signup.html'
+    context = {
+        'user_form' : user_form,
+        'profile_form' : profile_form,
+    }
+
+    def get(self, request, *args, **kwargs):
+        if request.user is not None:
+            logout(request)
+        user_form = self.user_form()
+        profile_form = self.profile_form()
+        return render(
+            request,
+            template_name = self.template_name,
+            context = self.context,
+        )
     
-    # Redirect to home if user successfully signs up
-    if request.method == 'POST':
-        user_form = SignUpForm(request.POST)
-        profile_form = ProfileForm(request.POST)
+    @method_decorator(transaction.atomic)
+    def post(self, request, *args, **kwargs):
+        user_form = self.user_form(request.POST)
+        profile_form = self.profile_form(request.POST)
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             username = user_form.cleaned_data.get('username')
             raw_password = user_form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
             login(request, user)
-            profile_form = ProfileForm(request.POST, instance=request.user.profile)
+            profile_form = self.profile_form(request.POST, instance=request.user.profile)
             profile_form.save()
             return redirect('home:home')
-    # Render the sign up form
-    else:
-        user_form = SignUpForm()
-        profile_form = ProfileForm()
-    return render(
-        request = request,
-        template_name = 'accounts/signup.html', 
-        context = {
-            'user_form' : user_form,
-            'profile_form' : profile_form,
-        }
-    )
+
 
 def userlogout(request):
     logout(request)
