4fdb3fcc5f85926349b753e3c313694c045f351b
Jerry Arciaga <jerryarciaga11@gmail.com>
2020-07-05 15:28:29 -0600

    Merged User and Profile Models in one admin page

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,4 +1,22 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+
 from .models import Profile
 
-admin.site.register(Profile)
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    vebose_name_plural = 'Profile'
+    fk_name = 'user'
+
+class CustomUserAdmin(UserAdmin):
+    inlines = (ProfileInline, )
+
+    def get_inline_instances(self, request, obj=None):
+        if not obj:
+            return list()
+        return super(CustomUserAdmin, self).get_inline_instances(request, obj)
+
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
