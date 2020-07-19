cd75a6d41fceaf72ce9f458b13556c6a97d4a594
Jerry Arciaga <jerryarciaga11@gmail.com>
2020-07-18 22:07:45 -0600

    Added new url pattern accounts:update_user

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     url(r'^signup/$', views.SignUpView.as_view(), name='signup'),
     url(r'^login/$', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
     url(r'^profile/$', views.UserProfileView.as_view(), name='profile'),
+    url(r'^update_user/$', views.UpdateUserView.as_view(), name='update_user'),
     url(r'^logout/$', views.userlogout, name='logout'),
 ]
