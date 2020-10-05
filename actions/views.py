30c28613d3f0c0a6cd13be7e57f40607a3bf585c
Jerry Arciaga <jerryarciaga11@gmail.com>
2020-10-05 23:54:01 +0000

    Actions WebApp is now requiring users to login

--- actions/views.py
+++ actions/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+from django.views import View
+
+class ActionsHome(View):
+    """ Renders a home view for the Actions WebApp """
+    template_name = 'actions/home.html'
+    
+    @method_decorator(login_required)
+    def get(self, request, *args, **kwargs):
+        return render(
+            request,
+            template_name = self.template_name,
+        )
